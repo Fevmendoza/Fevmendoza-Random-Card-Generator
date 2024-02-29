12f8af71ca54846e634203ea6ebf314a547a25e0
Fevmendoza <146396343+Fevmendoza@users.noreply.github.com>
2024-02-29 21:08:15 +0000

    100%

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function randomSuits() {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let ram = randomInteger(0, 3);
+  return suits[ram];
+}
+function randomNum() {
+  let randomNum = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let num = randomInteger(0, 12);
+  return randomNum[num];
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = randomSuits();
+  let num = randomNum();
+  let number = document.getElementById("number");
+  number.innerHTML = num;
+  let upSuit = document.getElementById("upSuit");
+  let downSuit = document.getElementById("downSuit");
+  upSuit.innerHTML = suit;
+  downSuit.innerHTML = suit;
+  document.getElementById("downSuit").innerHTML = suit;
+  if (suit === "♥" || suit === "♦") {
+    upSuit.style.color = "red";
+    downSuit.style.color = "red";
+  }
 };
